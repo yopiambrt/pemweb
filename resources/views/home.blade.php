aacdcdb4635f7a68458dbc7a351ec90d116d13b9
yopijunita07 <yopiambarita06@student.uns.ac.id>
2022-03-30 14:53:12 +0700

    lagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,16 +55,9 @@
           <li><a href="blog.html">Blog</a></li>
           <li class="dropdown"><a href="#"><span>Informasi</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Kompetisi Prestasi</a></li>
-                  <li><a href="#">Alumni</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Kompetisi Prestasi</a></li>
+              <li><a href="#">Alumni</a></li>
+        
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
